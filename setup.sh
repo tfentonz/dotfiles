33f15e25aca6d2f88863651e30ef0f2a70c10355
Tom Fenton <tom.fenton@xtra.co.nz>
2015-10-23 20:46:23 +1300

    Add setup.sh.

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SYMLINK_LIST="bash_profile
+bashrc
+gitconfig
+gitignore_global
+gvimrc
+inputrc
+npmrc
+rvmrc
+tmux.conf
+vim
+vimrc
+wgetrc"
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+cd $BASE_DIR
+
+for FILE in $SYMLINK_LIST
+do
+  rm -rf ~/.$FILE
+  ln -s $BASE_DIR/$FILE ~/.$FILE
+done
+
+git submodule init
+git submodule update
+
+# Install Vundle.vim Plugins.
+vim +PluginInstall +qall
+
+exit 0
